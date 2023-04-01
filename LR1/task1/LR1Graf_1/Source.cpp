37640d36b936b2abc760b8888f08c1779734e08d
Sergei1603 <serega_kuznetsov200@mail.ru>
2023-04-01 10:54:52 +0500

    Update Source.cpp

--- LR1/task1/LR1Graf_1/Source.cpp
+++ LR1/task1/LR1Graf_1/Source.cpp
@@ -1,27 +1,22 @@
-#include <GL/freeglut.h>
+#include <gl/freeglut.h> // подключение библиотеки glut 
 
-static void RenderSceneCB()
+void RenderSceneCB() // функция рендера
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glutSwapBuffers();
-}
-
-static void InitializeGlutCallbacks()
-{
-    glutDisplayFunc(RenderSceneCB);
+    glClear(GL_COLOR_BUFFER_BIT); // очистка буфера кадра
+    glutSwapBuffers(); // поменять местами фоновый буфер и буфер кадра
 }
 
 int main(int argc, char** argv)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowSize(1024, 768);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Tutorial 01");
+    glutInit(&argc, argv); // инициализация glut
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); // настройка опций glut
+    // двойная буферизация и буфер цвета для отображения на экране
+    glutInitWindowSize(1024, 768); // настройка размер отображаемого окна
+    glutInitWindowPosition(100, 100); // настройка положения окна
+    glutCreateWindow("Task 1"); // имя окна 
 
-    InitializeGlutCallbacks();
-
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-
-    glutMainLoop();
+    glutDisplayFunc(RenderSceneCB); // вызываем RenderScene
+    glClearColor(0.0f, 1.0f, 0.0f, 0.0f); // устанавливаем цвет, который
+    // будет использоваться во время очистки буфера кадра
+    glutMainLoop(); // передача управления glut
 }
